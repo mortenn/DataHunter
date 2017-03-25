b91ec48abf0407745c60ff137b49c6153f87e834
Morten Nilsen <mortenn@hapro.no>
2017-03-25 21:03:11 +0100

    Cleanup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,9 +5,6 @@ using DataHunter.ViewModel;
 
 namespace DataHunter
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow
 	{
 		public MainWindow()

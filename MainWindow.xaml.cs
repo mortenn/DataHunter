ed57c6bf35669c1255fb830f40be96b4ee7e9433
Morten Nilsen <mortenn@hapro.no>
2017-07-08 01:16:24 +0200

    Fix invalid cast exception on navigation using button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
 using DataHunter.ViewModel;
 
 namespace DataHunter
@@ -17,20 +15,15 @@ namespace DataHunter
 			Select(e.NewValue as DataContainer);
 		}
 
-		private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		private void NavigateAction(object sender, RoutedEventArgs e)
 		{
-			Select(((DataGridRow)sender).DataContext);
-		}
-
-		private void DataGrid_OnDoubleClick(object sender, MouseButtonEventArgs e)
-		{
-			Select(((DataGridRow)sender).DataContext);
+			Select(((FrameworkElement)sender).DataContext);
 		}
 
 		private void Select(object sender)
 		{
-			if(sender is DataContainer)
-				((AppContext)DataContext).SelectedFolder = (DataContainer)sender;
+			if(sender is DataContainer folder)
+				((AppContext)DataContext).SelectedFolder = folder;
 		}
 	}
 }
